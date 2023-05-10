af561de76372f5c05efaba1620e53c5026da82e9
front-chan <chan.jor.zz@gmail.com>
2023-05-10 22:41:54 +0900

    static / dynamic rendering and cache study

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,16 @@
 import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 
+export const dynamic = "force-dynamic";
+// 해당 페이지를 보여줄 때 dynamic rendering
+// dynamic rendering 단점 : 서버 / DB 부담이 커지므로 캐싱기능 사용 가능
+// 캐싱 : 결과를 잠깐 저장해두고 재사용
+// 페이지 캐싱 : 페이지 완성본을 잠깐 저장해두고 재사용
+// GET 요청 결과 캐싱 : GET 요청 결과를 잠깐 저장해두고 재사용
+
+// export const dynamic = 'force-static'
+// static rendering
+
 export default async function List() {
     const client = await connectDB;
     const db = client.db("forum");
--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
 import { connectDB } from "@/util/database";
 
+// export const revalidate = 60;
+// 예약변수를 쓰면 페이지단위 캐싱이 가능하다
+
 export default async function Home() {
     const client = await connectDB;
     const db = client.db("forum");
 
     let result = await db.collection("post").find().toArray();
     console.log(result);
+
+    // GET 요청 결과 캐싱
+    // await fetch("/URL", { cache: "force-cache" });
+    // 서버 자원 절약, 훨씬 빠르게 데이터를 가져올 수 있음
+    // URL까지만 써도 캐싱 가능
+
+    // cache : 'no-store'
+    // 캐싱하지 않고 매번 서버로 요청해서 새거 가져옴
+
+    // next : {revalidate : 60}
+    // 60초마다 캐싱된 데이터를 갱신해줌
+
+    // 캐싱된 데이터는 하드용량을 차지한다
 
     return <div>안녕</div>;
 }
