fc14d35e5cb01acb79b8bd40e6e859660b30bd36
front-chan <chan.jor.zz@gmail.com>
2023-05-15 11:51:47 +0900

    comment study

---
+++ app/detail/[id]/Comment.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Comment(props) {
+    let [comment, setComment] = useState("");
+
+    // useEffect(() => {
+    //     fetch();
+    // }, []);
+
+    return (
+        <div>
+            <div>댓글목록 보여줄 부분</div>
+            <input onChange={(e) => setComment(e.target.value)} />
+            <button
+                onClick={() => {
+                    console.log(comment);
+                    fetch("/api/comment/new", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            comment: comment,
+                            _id: props._id,
+                        }),
+                    });
+                }}
+            >
+                전송
+            </button>
+        </div>
+    );
+}
