1f9ebefd3d76444f48c185ec12565dce74fb9b6b
front-chan <chan.jor.zz@gmail.com>
2023-05-15 12:09:07 +0900

    comment study 2

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -4,14 +4,30 @@ import { useEffect, useState } from "react";
 
 export default function Comment(props) {
     let [comment, setComment] = useState("");
+    let [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     fetch();
-    // }, []);
+    useEffect(() => {
+        fetch(`/api/comment/list?id=${props._id}`)
+            .then((r) => r.json())
+            .then((result) => {
+                console.log(result);
+                setData(result);
+                console.log(data);
+                // setData가 console.log보다 늦게 출력되서 비어있을 수 있음
+            });
+    }, []);
 
     return (
         <div>
             <div>댓글목록 보여줄 부분</div>
+            <hr />
+            {data.length > 0
+                ? data.map((d) => (
+                      <div key={d._id}>
+                          <p>{d.content}</p>
+                      </div>
+                  ))
+                : "Loding..."}
             <input onChange={(e) => setComment(e.target.value)} />
             <button
                 onClick={() => {
